d4dea7b8060ce2914a62aa572a871edcf8aa1b08
Cynika <tgs9915@163.com>
2019-05-25 09:52:37 +0800

    Merge branch 'master' of https://github.com/circle-team/Circle
    # Conflicts:
    #	web/css/style.css

--- web/js/register.js
+++ web/js/register.js
@@ -1,11 +1,11 @@
 var Password_var;
-
+var Password2_var;
 var name_var;
 var reg_str = /^\w{6,20}$/;
 
 var name_ok;
 var Password_ok;
-
+var Password2_ok;
 
 function oBlur_name() {
     var name = document.getElementById("name").value;
@@ -13,9 +13,9 @@ function oBlur_name() {
         document.getElementById("alert_win").style.visibility="visible";
         document.getElementById("alert").innerHTML= "请输入昵称！";
     } else if (name.length > 20 || name.length < 6 || !reg_str.test(name))//用户框value值不为空
-        {
-            document.getElementById("alert_win").style.visibility="visible";
-            document.getElementById("alert").innerHTML= "昵称请输入6-20位字符包括数字字母，且不能包含特殊字符";
+    {
+        document.getElementById("alert_win").style.visibility="visible";
+        document.getElementById("alert").innerHTML= "昵称请输入6-20位字符包括数字字母，且不能包含特殊字符";
     }else{
         document.getElementById("alert_win").style.visibility="hidden";
         name_var=name;
@@ -41,13 +41,32 @@ function oBlur_Password() {
     }
 }
 
+function oBlur_Password2() {
+    var Password2 = document.getElementById("Password2").value;
+    if (Password_ok !="ok" )
+    {
+        document.getElementById("alert_win").style.visibility="visible";
+        document.getElementById("alert").innerHTML= "密码请输入6-20位字符包括数字字母，且不能包含特殊字符";
+    } else if (!Password2) { //密码框value值为空
+        document.getElementById("alert_win").style.visibility="visible";
+        document.getElementById("alert").innerHTML= "请再次输入密码！";
+    }else if(Password2 !=Password_var){
+        document.getElementById("alert_win").style.visibility="visible";
+        document.getElementById("alert").innerHTML= "两次密码输入不相同！";
+    }else{
+        document.getElementById("alert_win").style.visibility="hidden";
+        Password2_var=Password2;
+        Password2_ok="ok";
+    }
+}
+
 //若输入框为空，阻止表单的提交
 function submitTest() {
     if (name_ok !="ok") { //用户框value值和密码框value值都为空
-       alert("请按要求填写姓名");
-       return false;
+        alert("请按要求填写姓名");
+        return false;
 //只有返回true表单才会提交
-    } else if(Password_ok != "ok") { //用户框value值为空
+    } else if (Password2_ok != "ok" || Password_ok != "ok") { //用户框value值为空
         alert("请按要求填写密码");
         return false;
     } else if (Password2_ok !=Password_ok) { //密码框value值为空
