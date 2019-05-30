f3028e51a8b73185dec18c8b0173286a31225d26
Cynika <tgs9915@163.com>
2019-05-30 21:51:25 +0800

    Merge branch 'master' of https://github.com/circle-team/Circle
    # Conflicts:
    #	src/com/Dao/BlogInfoDao.java

--- web/js/comment.js
+++ web/js/comment.js
@@ -1,22 +1,31 @@
+
+
 $(document).ready(function(){
-    document.getElementById("alert_win").style.visibility="hidden";
+    document.getElementById("alert_win").style.display="none";
 })
 
 var comment_ok;
 var comment_var;
 
 function on_comment() {
-    var name = document.getElementById("comment").value;
-    if (!name) { //comment.value值为空
-        document.getElementById("alert_win").style.visibility="visible";
+    var comment = document.getElementById("comment").value;
+    if (!comment) { //comment.value值为空
+        document.getElementById("alert_win").style.display="";
         document.getElementById("alert").innerHTML= "请输入评论！";
-    } else if (name.length > 20 || name.length < 2)//用户框value值不为空
+    } else if (comment.length > 20 || comment.length < 2)//用户框value值不为空
     {
-        document.getElementById("alert_win").style.visibility="visible";
+        document.getElementById("alert_win").style.display="";
         document.getElementById("alert").innerHTML= "评论请输入6-20位字符";
     }else{
-        document.getElementById("alert_win").style.visibility="hidden";
-        comment_var=name;
+        document.getElementById("alert_win").style.display="none";
+        comment_var=comment;
         comment_ok="ok";
+    }
+}
+
+function submitTest() {
+    if (comment_ok !="ok") { //用户框value值和密码框value值都为空
+        alert("请按要求填写姓名");
+        return false;
     }
 }
