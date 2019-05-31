7d803f5a9d50c3664fdedf913b927243c6711cad
Sohyu1 <783487703@qq.com>
2019-05-31 13:09:27 +0800

    sl

--- web/js/home.js
+++ web/js/home.js
@@ -1,22 +1,4 @@
-// 导航栏动画效果
-var isAnimated = false;
-$(document).ready(function(){
-    $(window).on("scroll",function () {
-        if ($(this).scrollTop()>150) {
-            $(".box1").addClass("box2");
-            $(".box2").removeClass("box1");
-            if (!isAnimated) {
-                $(".navbar").css("top", "-60px");
-                $(".navbar").animate({"top": "0px"}, 1000);
-                isAnimated = true;
-            }
-        }else {
-            isAnimated = false;
-            $(".box1").removeClass("box2");
-            $(".box2").addClass("box1");
-        }
-    })
-})
+
 // 用户弹窗
     var oDiv1 = document.getElementById('user');
     var oDiv2 = document.getElementById('userText');
