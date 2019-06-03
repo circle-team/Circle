d5fdd089adc3ebe192df6320729741cb4f963e9e
cynika <tgs9915@163.com>
2019-06-03 13:47:08 +0800

    1.8

--- web/js/comment.js
+++ web/js/comment.js
@@ -1,7 +1,5 @@
-
-
-$(document).ready(function(){
-    document.getElementById("alert_win").style.display="none";
+$(document).ready(function () {
+    document.getElementById("alert_win").style.display = "none";
 })
 
 var comment_ok;
@@ -10,21 +8,21 @@ var comment_var;
 function on_comment() {
     var comment = document.getElementById("comment").value;
     if (!comment) { //comment.value值为空
-        document.getElementById("alert_win").style.display="";
-        document.getElementById("alert").innerHTML= "请输入评论！";
+        document.getElementById("alert_win").style.display = "";
+        document.getElementById("alert").innerHTML = "请输入评论！";
     } else if (comment.length > 20 || comment.length < 2)//用户框value值不为空
     {
-        document.getElementById("alert_win").style.display="";
-        document.getElementById("alert").innerHTML= "评论请输入6-20位字符";
-    }else{
-        document.getElementById("alert_win").style.display="none";
-        comment_var=comment;
-        comment_ok="ok";
+        document.getElementById("alert_win").style.display = "";
+        document.getElementById("alert").innerHTML = "评论请输入6-20位字符";
+    } else {
+        document.getElementById("alert_win").style.display = "none";
+        comment_var = comment;
+        comment_ok = "ok";
     }
 }
 
 function submitTest() {
-    if (comment_ok !="ok") { //用户框value值和密码框value值都为空
+    if (comment_ok != "ok") { //用户框value值和密码框value值都为空
         alert("请按要求填写姓名");
         return false;
     }
--- web/js/home.js
+++ web/js/home.js
@@ -1,8 +1,8 @@
 // 导航栏动画效果
 var isAnimated = false;
-$(document).ready(function(){
-    $(window).on("scroll",function () {
-        if ($(this).scrollTop()>150) {
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 150) {
             $(".box1").addClass("box2");
             $(".box2").removeClass("box1");
             if (!isAnimated) {
@@ -10,7 +10,7 @@ $(document).ready(function(){
                 $(".navbar").animate({"top": "0px"}, 1000);
                 isAnimated = true;
             }
-        }else {
+        } else {
             isAnimated = false;
             $(".box1").removeClass("box2");
             $(".box2").addClass("box1");
@@ -18,19 +18,19 @@ $(document).ready(function(){
     })
 })
 // 用户弹窗
-    var oDiv1 = document.getElementById('user');
-    var oDiv2 = document.getElementById('userText');
-    document.onclick = function(e) {
-        if( !oDiv1.contains(e.target)) {
-            oDiv2.style.display = "none";
-        }
-    }
-    oDiv1.onmouseover = function() {
-        oDiv2.style.display = "block";
-    }
-    oDiv1.onmouseout = function () {
+var oDiv1 = document.getElementById('user');
+var oDiv2 = document.getElementById('userText');
+document.onclick = function (e) {
+    if (!oDiv1.contains(e.target)) {
         oDiv2.style.display = "none";
     }
+}
+oDiv1.onmouseover = function () {
+    oDiv2.style.display = "block";
+}
+oDiv1.onmouseout = function () {
+    oDiv2.style.display = "none";
+}
 
 //遮罩窗体
 // var modal = document.getElementById("myModal");
@@ -53,56 +53,56 @@ $(document).ready(function(){
 
 //动画效果
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>110){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 110) {
             $('#part1').addClass(" fadeInLeft");
             $('#part1').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>170){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 170) {
             $('#part2').addClass(" fadeInDown")
             $('#part2').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>230){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 230) {
             $('#part3').addClass(" fadeInRight")
             $('#part3').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>300){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 300) {
             $('#part4').addClass(" fadeInRight")
             $('#part4').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>410){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 410) {
             $('#part5').addClass(" fadeInUp")
             $('#part5').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>420){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 420) {
             $('#part6').addClass(" fadeInUp")
             $('#part6').removeClass(" no");
         }
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>580){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 580) {
             $('#part7').addClass(" fadeInUp")
             $('#part7').removeClass(" no");
 
@@ -110,8 +110,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>700){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 700) {
             $('#part8').addClass(" fadeInLeft")
             $('#part8').removeClass(" no");
 
@@ -119,8 +119,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>700){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 700) {
             $('#part9').addClass(" fadeInUp")
             $('#part9').removeClass(" no");
 
@@ -128,8 +128,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>700){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 700) {
             $('#part10').addClass(" fadeInRight")
             $('#part10').removeClass(" no");
 
@@ -137,8 +137,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>700){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 700) {
             $('#part11').addClass(" fadeInUp")
             $('#part11').removeClass(" no");
 
@@ -146,8 +146,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>1200){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 1200) {
             $('#part12').addClass(" fadeInLeft")
             $('#part12').removeClass(" no");
 
@@ -155,8 +155,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>1200){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 1200) {
             $('#part13').addClass(" fadeInUp")
             $('#part13').removeClass(" no");
 
@@ -164,8 +164,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>1200){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 1200) {
             $('#part14').addClass(" fadeInRight")
             $('#part14').removeClass(" no");
 
@@ -173,8 +173,8 @@ $(document).ready(function () {
     })
 })
 $(document).ready(function () {
-    $(window).on("scroll",function () {
-        if($(this).scrollTop()>1200){
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 1200) {
             $('#part15').addClass(" fadeInUp")
             $('#part15').removeClass(" no");
 
