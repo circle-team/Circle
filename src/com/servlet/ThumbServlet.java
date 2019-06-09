c299515911b086cc228027a2f49ce85b3ebf6174
Yu <1749059433@qq.com>
2019-06-09 17:12:56 +0800

    1.9

--- src/com/servlet/ThumbServlet.java
+++ src/com/servlet/ThumbServlet.java
@@ -1,9 +1,12 @@
 package com.servlet;
 
+import com.entity.UserInfoEntity;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class ThumbServlet extends HttpServlet {
@@ -12,6 +15,9 @@ public class ThumbServlet extends HttpServlet {
         req.setCharacterEncoding("utf-8");
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
+        HttpSession session = req.getSession();
+        UserInfoEntity userinf =(UserInfoEntity)session.getAttribute("userinf");
+        Long id = userinf.getUid();
 
     }
 }
